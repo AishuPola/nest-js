1a83cc01bd0e48bf8f7bc84233c94ad494ed274f
AishuPola <aishwarya.pola@proclink.com>
2024-09-25 15:12:10 +0530

    methods implemented in service and checked

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
 import {
   Body,
   Controller,
@@ -7,34 +10,57 @@ import {
   Patch,
   Post,
   Query,
+  ParseIntPipe,
 } from '@nestjs/common'
-
+import { UsersService } from './users.service'
 @Controller('users') // /users
 // @ is the decorator.
 export class UsersController {
+  constructor(private readonly UsersService: UsersService) {}
   @Get() // get/ users or /users?role=value
   //users?role=value to get this, we use query params.
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return []
+    return this.UsersService.findAll(role)
   }
+  // @Get(':id') // Get users/:id
+  // findOne(@Param('id') id: string) {
+  //   return this.UsersService.findOne(+id)
+  // }
+
   @Get(':id') // Get users/:id
-  findOne(@Param('id') id: string) {
-    return { id }
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.UsersService.findOne(id)
   }
+
   @Get('intern')
   findAllInterns() {
     return []
   }
   @Post()
-  create(@Body() user: {}) {
-    return user
+  create(
+    @Body()
+    user: {
+      name: string
+      email: string
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN'
+    },
+  ) {
+    return this.UsersService.create(user)
   }
   @Patch(':id')
-  update(@Param('id') id: string, @Body() userupdate: {}) {
-    return { id, ...userupdate }
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body()
+    userupdate: {
+      name?: string
+      email?: string
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN'
+    },
+  ) {
+    return this.UsersService.update(id, userupdate)
   }
   @Delete(':id')
-  delete(@Param('id') id: string) {
-    return { id }
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.UsersService.delete(id)
   }
 }
