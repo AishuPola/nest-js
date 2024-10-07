9d0614b597265bc88e37998ace0bd36ffca254f6
AishuPola <aishwarya.pola@proclink.com>
2024-10-07 10:47:16 +0530

    installed passport library and generated auth m odule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 import { TasksModule } from './tasks/tasks.module'
 import { MongooseModule } from '@nestjs/mongoose'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
     UsersModule,
     TasksModule,
     MongooseModule.forRoot('mongodb://localhost:27017/nestjs_db'),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
