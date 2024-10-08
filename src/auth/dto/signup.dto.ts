7aa2a9033210f9ea04d964a6f3842747a41b6f43
AishuPola <aishwarya.pola@proclink.com>
2024-10-08 15:48:19 +0530

    added validators in signup dto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator'
+
 export class SignUpDto {
+  @IsNotEmpty()
+  @IsString()
   name: string
+  @IsNotEmpty()
+  @IsString()
   email: string
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  @MaxLength(15)
   password: string
   role: 'INTERN' | 'ENGINEER' | 'ADMIN'
 }
