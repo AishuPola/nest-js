b2dd2e7a0ee19f7f9d06281dabdfa1c6f21e8ca1
AishuPola <aishwarya.pola@proclink.com>
2024-10-22 11:08:07 +0530

    logging interceptor implemented to see how much it took to respond from the request

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { Logger } from '@nestjs/common'
 import * as dotenv from 'dotenv'
+import { LoggingInterceptor } from './interceptors/logging.interceptors'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalInterceptors(new LoggingInterceptor())
   dotenv.config()
   // Log the NODE_ENV value
   Logger.log(`NODE_ENV is set to: ${process.env.NODE_ENV}`)
