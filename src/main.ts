113c0450276bc829e0383695d1808c7c6aff39dd
AishuPola <aishwarya.pola@proclink.com>
2024-10-22 14:56:58 +0530

    transform interceptor implemented globally in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { ValidationPipe } from '@nestjs/common'
 import { Logger } from '@nestjs/common'
 import * as dotenv from 'dotenv'
 import { LoggingInterceptor } from './interceptors/logging.interceptors'
+import { TransformInterceptor } from './interceptors/transform.interceptors'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalInterceptors(new LoggingInterceptor())
+  app.useGlobalInterceptors(new TransformInterceptor())
   dotenv.config()
   // Log the NODE_ENV value
   Logger.log(`NODE_ENV is set to: ${process.env.NODE_ENV}`)
