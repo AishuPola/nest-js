2e65c4f75c39c96c8437ce8f919d7eab216bed6e
AishuPola <aishwarya.pola@proclink.com>
2024-10-22 15:51:47 +0530

    error interceptor implemented(gives 502 instead of 500)

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { ValidationPipe } from '@nestjs/common'
 import { Logger } from '@nestjs/common'
 import * as dotenv from 'dotenv'
 import { LoggingInterceptor } from './interceptors/logging.interceptors'
+import { ErrorsInterceptor } from './interceptors/errors.interceptors'
 //import { TransformInterceptor } from './interceptors/transform.interceptors'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalInterceptors(new LoggingInterceptor())
   //app.useGlobalInterceptors(new TransformInterceptor())
+  app.useGlobalInterceptors(new ErrorsInterceptor())
   dotenv.config()
   // Log the NODE_ENV value
   Logger.log(`NODE_ENV is set to: ${process.env.NODE_ENV}`)
