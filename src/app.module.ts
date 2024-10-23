1214729727403b1e5b2062ecea9ead940177684c
AishuPola <aishwarya.pola@proclink.com>
2024-10-23 11:40:31 +0530

    connected to mongodb atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module'
 import { TasksModule } from './tasks/tasks.module'
 import { MongooseModule } from '@nestjs/mongoose'
 import { AuthModule } from './auth/auth.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { BooksModule } from './books/books.module'
 import { APP_GUARD } from '@nestjs/core'
 import { RolesGuard } from './auth/guards/roles.guard'
@@ -18,7 +18,16 @@ import { RolesGuard } from './auth/guards/roles.guard'
       isGlobal: true, // Make ConfigModule globally available across the application
       envFilePath: '.env', // Specify the path to your .env file
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nestjs_db'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/nestjs_db'),
+    // AuthModule,
+    // BooksModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+    }),
     AuthModule,
     BooksModule,
   ],
